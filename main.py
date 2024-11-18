da9fd20eb8e3c548aaffd580e1fa5eda2508c12e
Mufeng Wang <mufeng.wang@mail.utoronto.ca>
2024-11-18 12:13:52 -0500

    modify output file name

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from generator import MathTestGenerator
 from latex_exporter import LatexExporter
 from pdf_exporter import PDFExporter
 from templates import generate_latex_document
+from datetime import datetime
+
+# Get the current date and time
+now = datetime.now()
 
 # Generate problems
+name = "Bella"
+date = now.strftime("%Y-%m-%d")  # Example format: "2024-11-17"
+
+
 test_generator = MathTestGenerator()
 test_generator.generate_addition_subtraction_100(num_problems=10) # 100以内加减法
 test_generator.generate_addition_subtraction_1000(num_problems=50) # 1000以内加减法
@@ -22,5 +30,5 @@ latex_problems, latex_solutions = LatexExporter.generate_latex(test_generator, s
 latex_document = generate_latex_document(latex_problems, latex_solutions)
 
 # Export to PDF
-pdf_exporter = PDFExporter(output_dir="output/")
+pdf_exporter = PDFExporter(output_dir=f"{name}_{date}/")
 pdf_exporter.export_to_pdf(latex_document, "math_test")
